fec9eb763ed68641d99cb958436313cb6aa0a62c
xiaozhou <xzhnfnu@163.com>
2025-01-09 10:08:22 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -17,12 +17,17 @@ extensions = [
    # 'numpydoc',
    'myst_parser',
    'sphinx.ext.duration',
+   'sphinx.ext.autodoc',
+   'sphinx.ext.napoleon',
+   'sphinx_copybutton',
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
+# 配置 copybutton 的行为, 设置不复制代码块前的提示符（如 ">>>", "$" 等）
+copybutton_prompt_text = r">>> |\$ "  # 正则表达式匹配提示符
+copybutton_only_copy_prompt_lines = False  # 如果提示符匹配，是否只复制带提示符的行
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
